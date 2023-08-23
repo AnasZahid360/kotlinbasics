1eaea430005300aafb42f92b05117e30281a539e
AnasZahid360 <anaszahid572@gmail.com>
2023-08-23 16:49:25 +0500

    23-Aug-23

---
+++ app/src/main/java/eu/tutorials/kotlinbasics/Basics.kt
@@ -0,0 +1,26 @@
+package eu.tutorials.kotlinbasics
+
+fun main() {
+    println("Hello, world!!!")
+    val myName = "Anas"
+
+    var myAge = 17
+
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 12_039_812_309_487_120
+
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.14159265358979323846
+
+    var isSunny: Boolean = true
+    isSunny = false
+
+    val letterChar = 'A'
+    val digitChar = '1'
+    
+    val myStr = "Hello World"
+    var firCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length - 1]
+}
